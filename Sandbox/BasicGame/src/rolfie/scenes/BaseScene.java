5d5a40b7841aa470416e2355f5c2000bc976a2a5
Rolf Wolf <rolfv415@gmail.com>
2023-11-22 20:28:57 +0100

    je dikke moedder

--- Sandbox/BasicGame/src/rolfie/scenes/BaseScene.java
+++ Sandbox/BasicGame/src/rolfie/scenes/BaseScene.java
@@ -3,6 +3,8 @@ package rolfie.scenes;
 import nl.saxion.app.SaxionApp;
 import rolfie.GameObject;
 
+import java.awt.event.KeyEvent;
+
 public class BaseScene extends Scene {
 
     public BaseScene() {
@@ -15,10 +17,32 @@ public class BaseScene extends Scene {
     public void init() {
         go = new GameObject(0, 0, SaxionApp.getWidth(), SaxionApp.getHeight(), "./assets/images/image.jpg");
     }
-
+    int x = SaxionApp.getWidth()/2;
+    int y = SaxionApp.getHeight()/2;
+    int radiusBall = 20;
     @Override
     public void update(boolean[] keysPressed) {
-        go.update();
+        SaxionApp.drawCircle(x, y, radiusBall);
+        if (keysPressed[KeyEvent.VK_W]) {
+            y -= 4;
+        } else if (keysPressed[KeyEvent.VK_S]) {
+            y += 4;
+        }
+        if (keysPressed[KeyEvent.VK_A]) {
+            x -= 4;
+        } else if (keysPressed[KeyEvent.VK_D]) {
+            x += 4;
+        }
+        if (x + radiusBall> SaxionApp.getWidth()) {
+            x -= 4;
+        } else if (x < +radiusBall) {
+            x += 4;
+        }
+        if (y + radiusBall > SaxionApp.getHeight()) {
+            y -= 4;
+        } else if (y < +radiusBall) {
+            y += 4;
+        }
     }
 
 
