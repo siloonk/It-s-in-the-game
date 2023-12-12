e226fdbaab703f25d46726f1f518921c069ae39d
Rolf Wolf <rolfv415@gmail.com>
2023-12-12 14:35:55 +0100

    Enemy spawnt op meerdere punten

---
+++ Sandbox/BasicGame/src/datajungle/components/Spawnpoint.java
@@ -0,0 +1,13 @@
+package datajungle.components;
+
+public class Spawnpoint {
+    public int x;
+    public int y;
+    public int direction;
+
+    public Spawnpoint(int x, int y, int direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+}
